8fb32370da27f3c107e04da243a55536e38ea82d
Recoon004 <124875971+Recoon004@users.noreply.github.com>
2023-03-16 12:06:57 +0100

    Create main.go

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	var i uint
+	ports := 0
+	for i = 1; i < 100; i++ {
+		addr := fmt.Sprintf("scanme.nmap.org:%d", i)
+		conn, err := net.Dial("tcp", addr)
+		if err == nil {
+			fmt.Println(i, "port is open")
+			conn.Close()
+			ports = ports + 1
+		} else {
+			fmt.Println(i, "port is closed")
+		}
+	}
+
+	fmt.Println("====================================")
+	fmt.Println("====================================")
+	fmt.Println("Scan is done")
+	fmt.Println(ports, "ports are open")
+	fmt.Println("====================================")
+	fmt.Println("====================================")
+}
